4e760ffbb62f53f9032d8fffc4db83bbcd827110
Todd S <tstewart1@wpi.edu>
2026-02-04 13:59:38 -0500

    Update walk-cycle-progress.js

--- walk-cycle-progress.js
+++ walk-cycle-progress.js
@@ -3,11 +3,12 @@
 // this system, one cycle is the timing to move one leg, ie 1 cycle would be 0.5 seconds
 
 let isRunning = false;
-let myFrame = 0;
+let myFrame = 0; /* mousePress frame by frame */
 
 let foot = {
   name: "fred",
   side: 1,     /* think 'left' or 'right' - but use an integer for id & sequence */
+  shoeSize: 50,
   
   xPos: -50,
   yPos: 300,
@@ -16,14 +17,17 @@ let foot = {
   stride: 2,
   
 //  holdTime: 30, /* hold time is just the other foot's swing time */
-  swingTime: 15, 
-  inTime: 3,
-  outTime: 3,
+  swingTime: 30, 
+  shiftTime: 3,
+  strikeTime: 3,
 
   holdSize: 1.0,
-  inSize: 0.8,
+  shiftSize: 0.8,
+  shiftInc: 0,
   swingSize: 0.5, 
-  outSize: 1.2,
+  swingInc: 0,
+  strikeSize: 1.2,
+  strikeInc: 0,
   
   swingApex: 1.3,
   swingDrag: 0.0   /* don't worry about drag for now */
@@ -39,17 +43,17 @@ function draw() {
   if (isRunning) {
   
   let cycle = {};
-
-  background(0);
+    
+  background(0)
 
   // a full cycleLength = sum of all feet per name 
   // #TODO - need to store/retrieve values of all feet per name #
   // for now, assume (a) only 2 feet -and- (b) each foot has identical timing
   
-  cycle.time = ( foot.inTime + foot.swingTime + foot.outTime ) ;
+  cycle.time = ( foot.shiftTime + foot.swingTime + foot.strikeTime ) ;
   
   // how many frames are we into the current cycle?
-  cycle.frameSeq = myFrame % cycle.time + 1;
+  cycle.frame = myFrame % cycle.time + 1; /* what frame of the cycle are we on? */
   
   // BEGIN with inTime and outTime 
   // FOOT 1 begins inTime, it's picking up, shifting weight to FOOT 2
@@ -58,22 +62,28 @@ function draw() {
   // incrementally over the first 3 frames of cycle
   
   // just do FOOT 2 for now 
-  
-  // here's where the current foot needs to reference the timing of the other foot to coordinate
-  // footSizes are relative to foot.size - they increase or decrease; foot.size -1 to get there
 
-  if ( cycle.frameSeq <= foot.inTime ) { 
-    // IN   FRAMES 1 through 3 
-    // this foot gets bigger as the other foot swings
-    foot.size = foot.size + ( foot.size * foot.outSize - foot.size ) / foot.outTime; 
+  let msg = "    local: " + cycle.frame + "   Size: " + foot.size;
+
+  if ( cycle.frame <= foot.shiftTime ) { 
+    // STRIKE FRAMES 1 through 3 
+    // 1 foot shifts / the other strikes
+
+    foot.strikeInc = round( ( ( foot.shoeSize * foot.strikeSize ) - 
+                              ( foot.shoeSize + foot.shiftInc * foot.shiftTime ) ) / 
+                                foot.strikeTime, 0);
+    
+    foot.size = foot.size + foot.strikeInc;
+        
     circle(width/2,height/2,foot.size);
-    console.log( "IN    local: " + cycle.frameSeq + "   frame: " + myFrame + "    Size: " + foot.size );
+
+    console.log("stage: STRIKE" + "    size: " + foot.size + "    inc: " + foot.strikeInc);
   } 
-    else if ( cycle.frameSeq <= foot.inTime + foot.swingTime ) {
+    else if ( cycle.frame <= foot.shiftTime + foot.swingTime ) {
     // SWING   FRAMES 4 though 33
     // this foot HOLDS while the other swings
     circle(width/2,height/2,foot.size);
-    console.log( "SWING    local: " + cycle.frameSeq + "   frame: " + myFrame + "    Size: " + foot.size );
+    console.log("stage: SWING" + "    size: " + foot.size + "    inc: 0");
   } 
     else {
       
@@ -82,15 +92,33 @@ function draw() {
     // need to know how many frames INTO this stage of the cycle we are
     // if we're at 34, this sequence is at local frame 1
       
-    let localFrame = ( cycle.frameSeq % ( foot.inTime + foot.swingTime ) )
-    // looks like  1 = ( 34 % ( 3 + 30 ) ) 
-    
-    foot.size =  foot.size + ( foot.size * foot.inSize - foot.size ) / foot.inTime; 
-      
+    foot.shiftInc = round( ( ( foot.shoeSize * foot.shiftSize) - 
+                             ( foot.shoeSize + foot.strikeInc * foot.strikeTime ) ) / 
+                               foot.shiftTime, 0);
+            
     circle(width/2,height/2,foot.size);
-    console.log( "OUT    local: " + cycle.frameSeq + "   frame: " + myFrame + "    Size: " + foot.size );
+
+    foot.size = foot.size + foot.shiftInc; 
+      
+    console.log("stage: SHIFT" + "    size: " + foot.size + "    inc: " + foot.shiftInc);
   }
-  
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   isRunning = !isRunning;
   myFrame++; 
   }
